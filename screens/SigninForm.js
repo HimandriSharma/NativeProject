71415aeac6a3531df87ddd024daac5d4aba26276
Himandri Sharma <himandrisharma27@gmail.com>
2022-12-17 00:19:07 +0530

    sign in/up set up successful

--- screens/SigninForm.js
+++ screens/SigninForm.js
@@ -1,10 +1,36 @@
 import React, {useState} from 'react';
 import {View, TextInput, Button} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const SignUpForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const createUser = () => {
+    auth()
+      .createUserWithEmailAndPassword(
+        'jane.doe@example.com',
+        'SuperSecretPassword!',
+      )
+      .then(() => {
+        console.log('User account created & signed in!');
+      })
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+        }
 
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
+
+        console.error(error);
+      });
+  };
+  const LogOut = () => {
+    auth()
+      .signOut()
+      .then(() => console.log('User signed out!'));
+  };
   return (
     <View>
       <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
@@ -14,10 +40,7 @@ const SignUpForm = () => {
         onChangeText={setPassword}
         secureTextEntry
       />
-      <Button
-        title="Sign In"
-        onPress={() => console.log(email, ' ', password)}
-      />
+      <Button title="Sign In" onPress={() => createUser()} />
     </View>
   );
 };
--- screens/SignupForm.js
+++ screens/SignupForm.js
@@ -1,23 +1,86 @@
-import React, {useState} from 'react';
-import {View, TextInput, Button} from 'react-native';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useState, useEffect} from 'react';
+import {View, TextInput, Button, Text} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const SignUpForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
 
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) {
+      setInitializing(false);
+    }
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) {
+    return null;
+  }
+  const createUser = () => {
+    console.log('button clicked');
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        console.log('User account created & signed in!');
+      })
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
+
+        console.error(error);
+      });
+  };
+  const LogOut = () => {
+    auth()
+      .signOut()
+      .then(() => console.log('User signed out!'));
+  };
   return (
     <View>
-      <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
+      {!user ? (
+        <>
+          <TextInput
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+          />
+          <TextInput
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+          />
+          <Button title="Sign Up" onPress={() => createUser()} />
+        </>
+      ) : (
+        <>
+          <Text>Welcome, {user.email}</Text>
+          <Button title="Log Out" onPress={() => LogOut()} />
+        </>
+      )}
+      {/* <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
       <TextInput
         placeholder="Password"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
       />
-      <Button
-        title="Sign Up"
-        onPress={() => console.log(email, ' ', password)}
-      />
+      <Button title="Sign Up" onPress={() => createUser()} />
+      <Button title="Log Out" onPress={() => LogOut()} /> */}
     </View>
   );
 };
